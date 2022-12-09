781ebeacac2fe79edc25e9dff61e81ea8bb8b637
xKerzi <boris.duque@neu.edu.ph>
2022-12-09 14:02:08 +0800

    2nd Activity
    Just 2nd Activity

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CS
 - Compilation of Java Programs. As a 1st Year Computer Science Student
 - 1st - Receiving User's input and printing output.
+- 2nd - Basic Calculator.
---
+++ cal.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class cal {
+    public void main(String[] args ) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        char operator = sc.next().charAt(0);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.print("What operator do you want to use? ");
+        
+        switch(operator){
+            case '+':
+                System.out.print("Added:");
+                System.out.print(x + y);
+                break;
+            case '-':
+                System.out.print("Subtracted:");
+                System.out.print(x - y);
+                break;
+            case '*': case 'x':
+                System.out.print("Multiplied:");
+                System.out.print(x*y);
+                break;
+            case '/': case '÷':
+                System.out.print("Get the remainder?");
+                char yn = sc.next().charAt(0);
+                if (yn == 'y'){
+                    System.out.print(x % y);
+                } else {
+                }
+                System.out.print("Divided by:");
+                System.out.print(x/y);
+                break;
+        }
+        sc.close();
+    
+    }
+}
