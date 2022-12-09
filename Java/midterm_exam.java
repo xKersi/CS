15479641e361886b7b501addfa55e22760f9009d
xKerzi <boris.duque@neu.edu.ph>
2022-12-09 14:46:55 +0800

    Added Midterm Examination

---
+++ Java/midterm_exam.java
@@ -0,0 +1,46 @@
+package Java;
+
+import java.util.*;
+
+//Identifying a specific input range.
+public class midterm_exam {
+    public void main(String[] args) throws Exception {
+        
+        //Defining Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Ranging Variable
+        int range = sc.next().charAt(0);
+
+        //Value identifying.
+        
+        //Range 1 - 50 divisible by 5 without remainders!
+        if (range >= 1 && range <= 50 && range % 5 == 0){
+            System.out.println(range + "Ranging 1 - 50 divisible by 5 withouot remainders!");
+
+            //Range 25 - 51 and 51 - 75 divisible by 2 without remainders!
+        } else if (range >= 51 && range <= 25 && range % 2 == 0 || range >= 51 && range <= 75 && range % 2 == 0){
+            System.out.println(range + "Ranging 25 - 75 divisible by 2 withouot remainders!");
+
+            //Range 1 -25 and 51 - 75 having remainders!
+        } else if ((range >= 1 && range <= 25 || range >= 51 && range <= 75)){
+            System.out.println(range + "Ranging 1 - 25 and 51 - 75 with remainders!");
+
+            //Range 26 - 50 and 76 - 100 divisible 2 without remainders!
+        } else if (( range >= 26 && range <= 50) || (range >= 76 && range <= 100 && range % 2 ==0)) {
+            System.out.print(range + "Ranging 26-50 and 76-100 divisible by 2 without remainder!");
+
+            //Range 26 - 50 and 76 - 100 having remainders!
+        } else if (range >= 26 && range <= 50 || range >= 76 && range <= 100) {
+            System.out.println(range + "Ranging 26-50 and 76-100 with remainders!");
+
+            //Out of Range!
+        } else {
+            System.out.print("OUT OF RANGE:");
+            System.out.print(range);
+        }
+
+        //Closing Scanner
+        sc.close();
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # CS
+~Note: Midterm Evaluation
 - Compilation of Java Programs. As a 1st Year Computer Science Student
-- 1st - Receiving User's input and printing output.
-[io.java]
-- 2nd - Basic Calculator.
-[cal.java]
+- 1st - Receiving User's input and printing output. FileName:[io.java]
+- 2nd - Basic Calculator. FileName:[cal.java]
 - 3rd - Using if and else in the past 2nd activity to make a Calculator
+- 4th - Midterm Examination. FileName:[miderm_exam.java]
+
+~Note: Final Evaluation
